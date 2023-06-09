63becc6b806683561b9bcd119f618d704d77dca9
Parth Bakhda <Bakhdaparth@gmail.comgit config --global user.email Bakhdaparth@gmail.comgit config --global user.email Parth>
2023-06-08 23:28:46 -0700

    Added webpack config

--- Develop/client/webpack.config.js
+++ Develop/client/webpack.config.js
@@ -3,8 +3,53 @@ const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
 
-// TODO: Add and configure workbox plugins for a service worker and manifest file.
-// TODO: Add CSS loaders and babel to webpack.
+const path = require('path');
+
+module.exports = {
+  entry: './script.js', // Specify the entry point (your main JavaScript file)
+  output: {
+    path: path.resolve(__dirname, 'dist'), // Specify the output path (where bundled files will be generated)
+    filename: 'bundle.js', // Specify the bundled JavaScript file name
+  },
+  module: {
+    rules: [
+      // Configure loaders for different file types
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader', // Use Babel loader for JavaScript files
+          options: {
+            presets: ['@babel/preset-env'], // Configure Babel presets if needed
+          },
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'], // Use style-loader and css-loader for CSS files
+      },
+      {
+        test: /\.html$/,
+        use: ['html-loader'], // Use html-loader for HTML files
+      },
+    ],
+  },
+  plugins: [
+    // Add any additional plugins for optimizations or other purposes
+    // For example, you can use the following plugin for minification
+    // new HtmlWebpackPlugin({
+    //   template: './index.html', // Specify the HTML template file
+    //   minify: {
+    //     collapseWhitespace: true,
+    //     removeComments: true,
+    //     removeRedundantAttributes: true,
+    //     removeScriptTypeAttributes: true,
+    //     removeStyleLinkTypeAttributes: true,
+    //     useShortDoctype: true,
+    //   },
+    // }),
+  ],
+};
 
 module.exports = () => {
   return {
